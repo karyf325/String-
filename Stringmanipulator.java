e5b8ca523f224237d8dc4d8b5d9f13974a49d15b
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-10-24 09:46:55 -0700

    Add files via upload

---
+++ Stringmanipulator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Karyme Flores String Manipulator 
+ *
+ */
+
+import java.util.Scanner; 
+
+public class Stringmanipulator {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(system.in); 
+		
+		
+		system.out.println("Enter any string:  "); 
+		String honey = kb.nextLine(); 
+		
+		
+
+}
